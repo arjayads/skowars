e96d80adaa84633f13a3776752590c4b0c66dc21
Arjay Ads <arjay@verticalops.com>
2015-07-08 01:23:24 +0800

    setup email

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,13 +1,14 @@
 class AccountActivationsController < ApplicationController
   def edit
     member = Member.find_by(email: params[:email])
+
     if member && !member.is_active && member.authenticated?(:activation, params[:id])
       member.activate
       # log_in member
-      flash[:success] = "Account activated!"
+      flash[:success] = 'Account activated!'
       redirect_to root_url
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = 'Invalid activation link'
       redirect_to root_url
     end
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,17 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      user_name:            ENV['GMAIL_USERNAME'],
+      password:             ENV['GMAIL_PASSWORD'],
+      authentication:       'plain',
+      openssl_verify_mode:  'none'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
