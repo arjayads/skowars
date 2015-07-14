546f309414dcd09c6ded5cfd90be87768995763a
Arjay Ads <arjay@verticalops.com>
2015-07-15 06:58:35 +0800

    remove serve_static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # For Heroku
-  config.serve_static_assets = true
 end
