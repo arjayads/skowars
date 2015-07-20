e50572c27620cd60db278268b0e06a377819a993
Arjay Ads <arjay@verticalops.com>
2015-07-21 06:49:17 +0800

    test activated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,9 +14,16 @@ class SessionsController < ApplicationController
   def create
     member = Member.find_by(email: params[:session][:email])
     if member && member.authenticate(params[:session][:password])
-      log_in member
-      params[:session][:remember_me] == '1' ? remember(member) : forget(member)
-      redirect_to member_profile_path
+      if member.is_active?
+        log_in member
+        params[:session][:remember_me] == '1' ? remember(member) : forget(member)
+        redirect_to member_profile_path
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:danger] = message
+        render 'signin'
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'signin'
